44a2c55b42c5bb01dc052cb9d1c54bb0cc3c4980
Stephan Wiesenhütter <stephan.wiesenhuetter@eulitha.com>
2022-02-18 11:06:59 +0100

    add a central widget (text edit), tree view with file system

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import sys
-from PySide6.QtWidgets import QApplication, QMainWindow, QDockWidget, QHBoxLayout, QTextEdit, QToolBar
+from PySide6.QtWidgets import QApplication, QTextEdit, QMainWindow, \
+    QDockWidget, QHBoxLayout, QTreeView, QToolBar, QFileSystemModel
 from PySide6.QtWebEngineWidgets import QWebEngineView
-from PySide6.QtCore import Qt, QUrl
+from PySide6.QtCore import Qt, QUrl, QDir
 from PySide6.QtGui import QIcon, QAction
 import CpuPlot
 
@@ -9,37 +10,53 @@ import CpuPlot
 class DockDemo(QMainWindow):
     def __init__(self, parent=None):
         super(DockDemo, self).__init__(parent)
+        self.setWindowIcon(QIcon('Eulitha_icon.ico'))
+        self.setWindowTitle('Eulitha Phabler')
+        central_widget = QTextEdit()
         layout = QHBoxLayout()
+        central_widget.setLayout(layout)
+        self.setCentralWidget(central_widget)
+
+        self.create_docks(central_widget)
+        self.create_menus()
+        self.create_toolbar()
+        self.statusBar().setStatusTip("Eulitha Phabler")
+
+    def create_menus(self):
         bar = self.menuBar()
         file_menu = bar.addMenu('File')
         file_menu.addAction('New')
         file_menu.addAction('Save')
         file_menu.addAction('Exit')
 
-        self.graph_dock = QDockWidget('Plot CPU %', self)
-        self.graph_widget = CpuPlot.CpuGraph(self.graph_dock)
-        self.graph_dock.setWidget(self.graph_widget)
-        self.graph_dock.setFloating(False)
-
-        self.web_dock = QDockWidget('Search Page', self)
-        self.webView = QWebEngineView()
-        self.webView.setUrl(QUrl('https://www.python.org'))
-        self.web_dock.setWidget(self.webView)
-        self.web_dock.setFloating(False)
-
-        self.setCentralWidget(QTextEdit())
-
         win_menu = bar.addMenu('Windows')
         win_menu.addAction(self.graph_dock.toggleViewAction())
         win_menu.addAction(self.web_dock.toggleViewAction())
 
+    def create_docks(self, parent):
+        self.graph_dock = QDockWidget('Plot CPU %', parent)
+        self.graph_widget = CpuPlot.CpuGraph(self.graph_dock)
+        self.graph_dock.setWidget(self.graph_widget)
+
+        self.web_dock = QDockWidget('Company Homepage', parent)
+        self.webView = QWebEngineView()
+        self.webView.setUrl(QUrl('https://www.eulitha.com'))
+        self.web_dock.setWidget(self.webView)
+
+        self.tree_dock = QDockWidget('Phabler Machine', parent)
+        self.tree_view = QTreeView()
+        self.tree_dock.setWidget(self.tree_view)
+
+        model = QFileSystemModel()
+        model.setRootPath(QDir.currentPath())
+        self.tree_view.setModel(model)
+        self.tree_view.setRootIndex(model.index(QDir.currentPath()))
+
+        self.addDockWidget(Qt.LeftDockWidgetArea, self.tree_dock)
         self.addDockWidget(Qt.RightDockWidgetArea, self.web_dock)
         self.addDockWidget(Qt.BottomDockWidgetArea, self.graph_dock)
 
-        self.setLayout(layout)
-        self.setWindowIcon(QIcon('Eulitha_icon.ico'))
-        self.setWindowTitle('Eulitha Phabler')
-
+    def create_toolbar(self):
         toolbar = QToolBar("Phabler Main Toolbar")
         self.addToolBar(toolbar)
         wec_action = QAction("WEC", self)
@@ -51,9 +68,6 @@ class DockDemo(QMainWindow):
         exp_action.setStatusTip("DTL Exposure")
         exp_action.triggered.connect(self.click_exp)
         toolbar.addAction(exp_action)
-
-        self.statusBar().setStatusTip("Eulitha Phabler")
-
 
     def click_wec(self):
         print("WEC")
